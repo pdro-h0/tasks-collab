99f4a24fe3a7990127df4e8b31d2683c8929285b
Pedro Henrique de Oliveira <pedro.pho792@gmail.com>
2025-12-02 16:05:50 -0300

    feat: add task-service

---
+++ apps/tasks-service/src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { DbModule } from './db/db.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DbModule,
+    TasksModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
