153eed61ecfc72a68b0e9d446fb14f035f1eabdb
Pedro Henrique de Oliveira <pedro.pho792@gmail.com>
2025-12-02 15:33:11 -0300

    feat: create User entity

---
+++ apps/auth-service/src/db/entities/user.entity.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryColumn('uuid')
+  id: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar' })
+  password: string;
+
+  @CreateDateColumn({ type: 'timestamp with time zone', name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamp with time zone', name: 'updated_at' })
+  updatedAt: Date;
+}
