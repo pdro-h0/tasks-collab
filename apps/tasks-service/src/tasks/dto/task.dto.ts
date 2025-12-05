3e526c103ac7358c77988df303d535cffcb7b944
Pedro Henrique de Oliveira <pedro.pho792@gmail.com>
2025-12-05 17:25:13 -0300

    fix: ensure sueDate has correct type

--- apps/tasks-service/src/tasks/dto/task.dto.ts
+++ apps/tasks-service/src/tasks/dto/task.dto.ts
@@ -1,81 +1,81 @@
 import {
-  IsString,
-  IsNotEmpty,
-  IsDateString,
-  IsArray,
-  IsOptional,
-  IsIn,
-  IsUUID,
-  ValidateNested,
+    IsString,
+    IsNotEmpty,
+    IsDateString,
+    IsArray,
+    IsOptional,
+    IsIn,
+    IsUUID,
+    ValidateNested,
 } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class CreateTaskDto {
-  @IsString()
-  @IsNotEmpty()
-  title: string;
+    @IsString()
+    @IsNotEmpty()
+    title: string;
 
-  @IsString()
-  @IsNotEmpty()
-  description: string;
+    @IsString()
+    @IsNotEmpty()
+    description: string;
 
-  @IsIn(['LOW', 'MEDIUM', 'HIGH', 'URGENT'])
-  priority: 'LOW' | 'MEDIUM' | 'HIGH' | 'URGENT';
+    @IsIn(['LOW', 'MEDIUM', 'HIGH', 'URGENT'])
+    priority: 'LOW' | 'MEDIUM' | 'HIGH' | 'URGENT';
 
-  @IsDateString()
-  dueDate: string | Date;
+    @IsDateString()
+    dueDate: Date;
 
-  @IsArray()
-  @IsOptional()
-  @IsUUID('4', { each: true })
-  assignedUserIds?: string[];
+    @IsArray()
+    @IsOptional()
+    @IsUUID('4', { each: true })
+    assignedUserIds?: string[];
 
-  @IsString()
-  @IsUUID('4')
-  authorId: string;
+    @IsString()
+    @IsUUID('4')
+    authorId: string;
 }
 
 class UpdateTaskDataDto {
-  @IsString()
-  @IsOptional()
-  title?: string;
+    @IsString()
+    @IsOptional()
+    title?: string;
 
-  @IsString()
-  @IsOptional()
-  description?: string;
+    @IsString()
+    @IsOptional()
+    description?: string;
 
-  @IsIn(['TODO', 'IN_PROGRESS', 'REVIEW', 'DONE'])
-  @IsOptional()
-  status?: 'TODO' | 'IN_PROGRESS' | 'REVIEW' | 'DONE';
+    @IsIn(['TODO', 'IN_PROGRESS', 'REVIEW', 'DONE'])
+    @IsOptional()
+    status?: 'TODO' | 'IN_PROGRESS' | 'REVIEW' | 'DONE';
 
-  @IsIn(['LOW', 'MEDIUM', 'HIGH', 'URGENT'])
-  @IsOptional()
-  priority?: 'LOW' | 'MEDIUM' | 'HIGH' | 'URGENT';
+    @IsIn(['LOW', 'MEDIUM', 'HIGH', 'URGENT'])
+    @IsOptional()
+    priority?: 'LOW' | 'MEDIUM' | 'HIGH' | 'URGENT';
 
-  @IsDateString()
-  @IsOptional()
-  dueDate?: string | Date;
+    @IsDateString()
+    @IsOptional()
+    dueDate?: Date;
 
-  @IsArray()
-  @IsOptional()
-  @IsUUID('4', { each: true })
-  assignedUserIds?: string[];
+    @IsArray()
+    @IsOptional()
+    @IsUUID('4', { each: true })
+    assignedUserIds?: string[];
 }
 
 export class UpdateTaskDto {
-  @IsString()
-  @IsUUID('4')
-  id: string;
+    @IsString()
+    @IsUUID('4')
+    id: string;
 
-  @Type(() => UpdateTaskDataDto)
-  @ValidateNested()
-  taskData: UpdateTaskDataDto;
+    @Type(() => UpdateTaskDataDto)
+    @ValidateNested()
+    taskData: UpdateTaskDataDto;
 }
 
 export class TaskIdParamDto {
-  @IsString()
-  @IsUUID('4')
-  id: string;
+    @IsString()
+    @IsUUID('4')
+    id: string;
 }
 
 export class GetTaskByIdDto extends TaskIdParamDto {}
